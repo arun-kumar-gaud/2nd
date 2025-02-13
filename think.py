39532d9ecad295c3675e6edf94f79275a8e2c2b5
Arun Kumar Gaud <arunkumargaud@gmail.com>
2025-02-13 13:54:02 +0530

    removed extra database url

--- think.py
+++ think.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 
 # Database setup
-SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL  = os.getenv("DATABASE_URL")
 if SQLALCHEMY_DATABASE_URL and SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
     SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
